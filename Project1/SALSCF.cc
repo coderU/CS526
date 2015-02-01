4f89add89c084ededad262f7c11c13927731dc0f
coderU <jemit.uriel@gmail.com>
2015-02-01 10:24:11 -0800

    lol

--- Project1/SALSCF.cc
+++ Project1/SALSCF.cc
@@ -45,67 +45,86 @@ vector<string> split(string orignal, char target){
   return ret;
 }
 
-string Hash(const char* target){
-  unsigned char digest[20];
-  char *str = (char*)target;
-  //char str[] = "dasda";
-  SHA1( (unsigned char*)str, strlen(str), (unsigned char*)&digest );    
- 
-  char mdString[20];
+string Hash(const char* string){
   
-  for(int i = 0; i < 19; i++)
-    sprintf( &mdString[i], "%02x", (unsigned int)digest[i] );
+  char outputBuffer[64];
+  unsigned char hash[SHA256_DIGEST_LENGTH];
+  SHA256_CTX sha256;
+  SHA256_Init(&sha256);
+  SHA256_Update(&sha256, string, strlen(string));
+  SHA256_Final(hash, &sha256);
+  int i = 0;
+  for(i = 0; i < SHA256_DIGEST_LENGTH; i++)
+    {
+      sprintf(outputBuffer + (i * 2), "%02x", hash[i]);
+    }
+  outputBuffer[64] = 0;
   
-  string ret(mdString);
+  std::string ret(outputBuffer);
   return ret;
 }
 
-char *
-Encrypt( char *Key, char *Msg, int size)
+int aes_init(unsigned char *key_data, int key_data_len, unsigned char *salt, EVP_CIPHER_CTX *e_ctx, 
+             EVP_CIPHER_CTX *d_ctx)
 {
- 
-  static char*    Res;
-  int             n=0;
-  DES_cblock      Key2;
-  DES_key_schedule schedule;
- 
-  Res = ( char * ) malloc( size );
- 
-  /* Prepare the key for use with DES_cfb64_encrypt */
-  memcpy( Key2, Key,8);
-  DES_set_odd_parity( &Key2 );
-  DES_set_key_checked( &Key2, &schedule );
- 
-  /* Encryption occurs here */
-  DES_cfb64_encrypt( ( unsigned char * ) Msg, ( unsigned char * ) Res,
-		     size, &schedule, &Key2, &n, DES_ENCRYPT );
- 
-  return (Res);
+  int i, nrounds = 8;
+  unsigned char key[32], iv[32];
+  
+  /*
+   * Gen key & IV for AES 256 CBC mode. A SHA1 digest is used to hash the supplied key material.
+   * nrounds is the number of times the we hash the material. More rounds are more secure but
+   * slower.
+   */
+  i = EVP_BytesToKey(EVP_aes_256_cbc(), EVP_sha1(), salt, key_data, key_data_len, nrounds, key, iv);
+  if (i != 32) {
+    printf("Key size is %d bits - should be 256 bits\n", i);
+    return -1;
+  }
+
+  EVP_CIPHER_CTX_init(e_ctx);
+  EVP_EncryptInit_ex(e_ctx, EVP_aes_256_cbc(), NULL, key, iv);
+  EVP_CIPHER_CTX_init(d_ctx);
+  EVP_DecryptInit_ex(d_ctx, EVP_aes_256_cbc(), NULL, key, iv);
+
+  return 0;
 }
 
 char *
-Decrypt( char *Key, char *Msg, int size)
-{
- 
-  static char*    Res;
-  int             n=0;
- 
-  DES_cblock      Key2;
-  DES_key_schedule schedule;
- 
-  Res = ( char * ) malloc( size );
- 
-  /* Prepare the key for use with DES_cfb64_encrypt */
-  memcpy( Key2, Key,8);
-  DES_set_odd_parity( &Key2 );
-  DES_set_key_checked( &Key2, &schedule );
- 
-  /* Decryption occurs here */
-  DES_cfb64_encrypt( ( unsigned char * ) Msg, ( unsigned char * ) Res,
-		     size, &schedule, &Key2, &n, DES_DECRYPT ); 
-  return (Res);
+Encrypt(EVP_CIPHER_CTX *e, unsigned char *plaintext, int *len)
+{  /* max ciphertext len for a n bytes of plaintext is n + AES_BLOCK_SIZE -1 bytes */
+  int c_len = *len + AES_BLOCK_SIZE, f_len = 0;
+  unsigned char *ciphertext = (unsigned char*)malloc(c_len);
+
+  /* allows reusing of 'e' for multiple encryption cycles */
+  EVP_EncryptInit_ex(e, NULL, NULL, NULL, NULL);
+
+  /* update ciphertext, c_len is filled with the length of ciphertext generated,
+   *len is the size of plaintext in bytes */
+  EVP_EncryptUpdate(e, ciphertext, &c_len, plaintext, *len);
+
+  /* update ciphertext with the final remaining bytes */
+  EVP_EncryptFinal_ex(e, ciphertext+c_len, &f_len);
+
+  *len = c_len + f_len;
+  return (char*)ciphertext;
 
 }
+
+char *
+Decrypt(EVP_CIPHER_CTX *e, unsigned char *ciphertext, int *len)
+{
+  /* plaintext will always be equal to or lesser than length of ciphertext*/
+  int p_len = *len, f_len = 0;
+  unsigned char *plaintext = (unsigned char*)malloc(p_len);
+  
+  EVP_DecryptInit_ex(e, NULL, NULL, NULL, NULL);
+  EVP_DecryptUpdate(e, plaintext, &p_len, ciphertext, *len);
+  EVP_DecryptFinal_ex(e, plaintext+p_len, &f_len);
+
+  *len = p_len + f_len;
+  return (char*)plaintext;
+}
+
 
 
 std::string string_to_hex(const std::string& input)
@@ -207,24 +226,41 @@ void createlog(string filename){
   idlog = filename;
   LastA = A0;
   
-  string key = Hash( (W[0]+A0).c_str() );
+
   
-  if(DEBUG)
-    cout<<"Key for D0 is: "<<key<<", with size: "<<strlen(key.c_str())<<endl;
-  
+
   string D0( ctime(&d) );
   string temp( ctime(&dplus) );
   D0 = D0 + temp;
   D0 += idlog; 
 
-  string encipher = Encrypt( (char*)key.c_str(), (char*)D0.c_str(), D0.size());
 
-  if(DEBUG){
-    cout<<"---------------\nEncrypt data: \n"<<string_to_hex(encipher)<<endl;
-    string decipher = Decrypt( (char*)key.c_str(), (char*)encipher.c_str(), D0.size());
-    cout<<"---------------\nDecrypt data: \n"<<decipher<<endl;
+  //################################################
+  unsigned int salt[] = {12345, 54321};
+  string key_data = Hash( (W[0]+A0).c_str() );
+  int key_data_len = key_data.length();
+  int plaintext_len = D0.length()+1;
+  EVP_CIPHER_CTX en, de;
+
+  if(DEBUG)
+    cout<<"Key for D0 is: "<<key_data<<", with size: "<<key_data_len<<endl;
+    
+  if (aes_init( (unsigned char*)key_data.c_str(), key_data_len, (unsigned char *)&salt, &en, &de)) {
+    printf("Couldn't initialize AES cipher\n");
+    return;
   }
 
+  
+  unsigned char* ciphertext = (unsigned char*)Encrypt(&en, (unsigned char*)D0.c_str(), &plaintext_len);
+  string encipher( (const char*)ciphertext);
+
+  //###############################################
+  if(DEBUG){
+    cout<<"---------------\nEncrypt data: \n"<<string_to_hex(encipher)<<endl;
+    string decipher = string( Decrypt( &de, ciphertext, &plaintext_len) );
+    cout<<"---------------\nDecrypt data: \n"<<decipher<<endl;
+  }
+  //###############################################
   
   string y = Hash( (encipher+W[0]).c_str() );
   if(DEBUG)
@@ -235,7 +271,7 @@ void createlog(string filename){
   unsigned int zlength = 20;
   HMAC_CTX ctx;
   HMAC_CTX_init(&ctx);
-  HMAC_Init_ex(&ctx,A0.c_str(),20,EVP_sha1(),NULL);
+  HMAC_Init_ex(&ctx,A0.c_str(),20,EVP_sha256(),NULL);
   HMAC_Update(&ctx,(const unsigned char *)y.c_str(),20);
   HMAC_Final(&ctx,(unsigned char *)tempZ,&zlength);
 
@@ -268,13 +304,36 @@ void add(string message){
 
   string A = Hash( LastA.c_str() );
   string Df = message;
-  string key = Hash( (W[1]+A).c_str() );
-  
-  string encipher = Encrypt( (char*)key.c_str(), (char*)Df.c_str(), Df.size());
 
+  
+
+
+
+  
+  //################################################
+  unsigned int salt[] = {12345, 54321};
+  string key_data = Hash( (W[1]+A).c_str() );
+  int key_data_len = key_data.length();
+  int plaintext_len = Df.length()+1;
+  EVP_CIPHER_CTX en, de;
+
+  if(DEBUG)
+    cout<<"Key for Df is: "<<key_data<<", with size: "<<key_data_len<<endl;
+    
+  if (aes_init( (unsigned char*)key_data.c_str(), key_data_len, (unsigned char *)&salt, &en, &de)) {
+    printf("Couldn't initialize AES cipher\n");
+    return;
+  }
+
+  
+  unsigned char* ciphertext = (unsigned char*)Encrypt(&en, (unsigned char*)Df.c_str(), &plaintext_len);
+  string encipher( (const char*)ciphertext);
+
+  //###############################################
+  
   if(DEBUG){
     cout<<"---------------\nEncrypt data: \n"<<string_to_hex(encipher)<<endl;
-    string decipher = Decrypt( (char*)key.c_str(), (char*)encipher.c_str(), Df.size());
+    string decipher = string( Decrypt( &de, ciphertext, &plaintext_len) );
     cout<<"---------------\nDecrypt data: \n"<<decipher<<endl;
   }
 
@@ -288,7 +347,7 @@ void add(string message){
   unsigned int zlength = 20;
   HMAC_CTX ctx;
   HMAC_CTX_init(&ctx);
-  HMAC_Init_ex(&ctx,A.c_str(),20,EVP_sha1(),NULL);
+  HMAC_Init_ex(&ctx,A.c_str(),20,EVP_sha256(),NULL);
   HMAC_Update(&ctx,(const unsigned char *)y.c_str(),20);
   HMAC_Final(&ctx,(unsigned char *)tempZ,&zlength);
 
@@ -306,13 +365,37 @@ void add(string message){
 void closelog(){
   string A = Hash( LastA.c_str() );
   string Df = "Close";
-  string key = Hash( (W[2]+A).c_str() );
+
+
+
+
+
+  //################################################
+  unsigned int salt[] = {12345, 54321};
+  string key_data = Hash( (W[2]+A).c_str() );
+  int key_data_len = key_data.length();
+  int plaintext_len = Df.length()+1;
+  EVP_CIPHER_CTX en, de;
+
+  if(DEBUG)
+    cout<<"Key for Df is: "<<key_data<<", with size: "<<key_data_len<<endl;
+    
+  if (aes_init( (unsigned char*)key_data.c_str(), key_data_len, (unsigned char *)&salt, &en, &de)) {
+    printf("Couldn't initialize AES cipher\n");
+    return;
+  }
+
   
-  string encipher = Encrypt( (char*)key.c_str(), (char*)Df.c_str(), Df.size());
+  unsigned char* ciphertext = (unsigned char*)Encrypt(&en, (unsigned char*)Df.c_str(), &plaintext_len);
+  string encipher( (const char*)ciphertext);
+
+  //###############################################
+  
+
 
   if(DEBUG){
     cout<<"---------------\nEncrypt data: \n"<<string_to_hex(encipher)<<endl;
-    string decipher = Decrypt( (char*)key.c_str(), (char*)encipher.c_str(), Df.size());
+    string decipher = string( Decrypt( &de, ciphertext, &plaintext_len) );
     cout<<"---------------\nDecrypt data: \n"<<decipher<<endl;
   }
 
@@ -326,7 +409,7 @@ void closelog(){
   unsigned int zlength = 20;
   HMAC_CTX ctx;
   HMAC_CTX_init(&ctx);
-  HMAC_Init_ex(&ctx,A.c_str(),20,EVP_sha1(),NULL);
+  HMAC_Init_ex(&ctx,A.c_str(),20,EVP_sha256(),NULL);
   HMAC_Update(&ctx,(const unsigned char *)y.c_str(),20);
   HMAC_Final(&ctx,(unsigned char *)tempZ,&zlength);
 
@@ -406,7 +489,7 @@ void verify(int number){
     unsigned int zlength = 20;
     HMAC_CTX ctx;
     HMAC_CTX_init(&ctx);
-    HMAC_Init_ex(&ctx,A.c_str(),20,EVP_sha1(),NULL);
+    HMAC_Init_ex(&ctx,A.c_str(),20,EVP_sha256(),NULL);
     HMAC_Update(&ctx,(const unsigned char *)y.c_str(),20);
     HMAC_Final(&ctx,(unsigned char *)tempZ,&zlength);
     
@@ -417,8 +500,32 @@ void verify(int number){
 
 
     if( !z.compare(zC) ){
-      string key = Hash( (w+A).c_str() );
-      string decipher = Decrypt( (char*)key.c_str(), (char*)encipher.c_str(), message_size);
+      
+      //################################################
+      unsigned int salt[] = {12345, 54321};
+      string key_data = Hash( (w+A).c_str() );
+      int key_data_len = key_data.length();
+      int ciphertext_len = encipher.length()+1;
+      EVP_CIPHER_CTX en, de;
+      
+      if(DEBUG)
+	cout<<"Key for Decipher: "<<number<<" is: "<<key_data<<", with size: "<<key_data_len<<endl;
+      
+      if (aes_init( (unsigned char*)key_data.c_str(), key_data_len, (unsigned char *)&salt, &en, &de)) {
+	printf("Couldn't initialize AES cipher\n");
+	return;
+      }
+      
+      
+      string decipher = string( Decrypt( &de, (unsigned char*)encipher.c_str(), &ciphertext_len) );
+      
+	  
+      
+      //###############################################
+      
+
+
+      
       cout<<"Message at line: "<<number<<" is:\n-----------------\n"<<decipher<<"\n-----------------"<<endl;
     }else{
       cout<<"Failed verication"<<endl;
@@ -457,6 +564,7 @@ void verifyall(string infile, string outfile){
 
   if (myfile.is_open())
     {
+      int count = 0;
       while ( getline (myfile,line) ){
 
 
@@ -472,7 +580,7 @@ void verifyall(string infile, string outfile){
 	unsigned int zlength = 20;
 	HMAC_CTX ctx;
 	HMAC_CTX_init(&ctx);
-	HMAC_Init_ex(&ctx,A.c_str(),20,EVP_sha1(),NULL);
+	HMAC_Init_ex(&ctx,A.c_str(),20,EVP_sha256(),NULL);
 	HMAC_Update(&ctx,(const unsigned char *)y.c_str(),20);
 	HMAC_Final(&ctx,(unsigned char *)tempZ,&zlength);
 	
@@ -481,10 +589,39 @@ void verifyall(string infile, string outfile){
 	if(DEBUG)
 	  cout<<"-----------------\n"<<"A: "<<string_to_hex(A)<<endl<<"Y: "<<y<<endl<<"Z: "<<string_to_hex(zC)<<"\n-----------------\n";
 	
+
+
 	
 	if( !z.compare(zC) ){
 	  string key = Hash( (w+A).c_str() );
-	  string decipher = Decrypt( (char*)key.c_str(), (char*)encipher.c_str(), message_size);
+
+	  
+	  
+	  //################################################
+	  unsigned int salt[] = {12345, 54321};
+	  string key_data = Hash( (w+A).c_str() );
+	  int key_data_len = key_data.length();
+	  int ciphertext_len = encipher.length()+1;
+	  EVP_CIPHER_CTX en, de;
+      
+	  if(DEBUG)
+	    cout<<"Key for Decipher: "<<count++<<" is: "<<key_data<<", with size: "<<key_data_len<<endl;
+	  
+	  if (aes_init( (unsigned char*)key_data.c_str(), key_data_len, (unsigned char *)&salt, &en, &de)) {
+	    printf("Couldn't initialize AES cipher\n");
+	    return;
+	  }
+	  
+	  
+	  string decipher = string( Decrypt( &de, (unsigned char*)encipher.c_str(), &ciphertext_len) );
+	  
+	  
+
+	  //###############################################
+        
+
+
+
 	  if( !w.compare(W[0]) || !w.compare(W[2]) ){
 	      
 	  }else{
@@ -516,6 +653,8 @@ int main(int argc, char* argv[]){
   while(true){
     cout<<"Please choose a command: createlog, add, closelog, verify, verifyall, exit."<<endl;
     getline(cin,commands);
+    if(commands.compare("") == 0)
+      continue;
     commandList = split(commands,' ');
 
     string command = commandList.at(0);
@@ -529,8 +668,16 @@ int main(int argc, char* argv[]){
     }else if( command.compare("add") == 0 ){
       if(DEBUG)
 	cout<<"Command: add"<<endl;
-      if(lock)
-	add(commandList.at(1) );
+      if(lock){
+	string message = "";
+	for(int i = 1 ; i < commandList.size() ; i++){
+	  message.append( commandList.at(i) );
+	  if( i != commandList.size() - 1)
+	    message.append( " " );
+	}
+
+	add( message);
+      }
     }else if( command.compare("closelog") == 0 ){
       if(DEBUG)
 	cout<<"Command: closelog"<<endl;
--- Project1/gen_keys.sh
+++ Project1/gen_keys.sh
@@ -2,4 +2,4 @@
 
 openssl genrsa -out proj3.pri 1024
 openssl rsa -in proj3.pri -pubout > proj3.pub
-openssl rand -out proj3.symm 20
+openssl enc -aes-256-cbc -k secret -P -md sha1
